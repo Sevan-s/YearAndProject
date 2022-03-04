4831b50e43c043e2b25f6eda46508ede228ee64f
nathan-hoche <hochenat@gmail.com>
2022-03-04 16:08:07 +0100

    [FIX]: oauth google doesn't get username

--- Web/src/js/Auth.js
+++ Web/src/js/Auth.js
@@ -11,7 +11,8 @@ function signIn(email, password) {
 }
 
 const responseGoogle = (response) => {
-  axios.post("http://localhost:8080/user/oauth/", {"username": response["profileObj"]["username"], "password": response["profileObj"]["googleId"], "OAUTH": true})
+  console.log(response)
+  axios.post("http://localhost:8080/user/oauth/", {"username": response["profileObj"]["name"], "password": response["profileObj"]["googleId"], "OAUTH": true})
   axios.post("http://localhost:8080/user/setAccountLink/", {"token": response["accessToken"], "name": "Google"})
   window.location.reload(false);
 }
